69230879a785bd60d0c503830cf11fc25f2c579f
Lu <kechenl3@fa18-cs425-g29-10.cs.illinois.edu>
2018-09-15 19:05:22 -0500

    fix a bug
    Signed-off-by: Lu <kechenl3@fa18-cs425-g29-10.cs.illinois.edu>

--- scripts/start_server_remote.sh
+++ scripts/start_server_remote.sh
@@ -2,11 +2,11 @@
 
 for val in {1..9}
 do
-    ssh kechenl3@fa18-cs425-g29-0$val.cs.illinois.edu "nohup ~/go/src/dist-grep/scripts/start_server_local.sh"
+    ssh kechenl3@fa18-cs425-g29-0$val.cs.illinois.edu "~/go/src/dist-grep/scripts/start_server_local.sh"
     echo VM$val Server Up
 done
 
-ssh kechenl3@fa18-cs425-g29-10.cs.illinois.edu "nohup ~/go/src/dist-grep/scripts/start_server_local.sh"
+ssh kechenl3@fa18-cs425-g29-10.cs.illinois.edu "~/go/src/dist-grep/scripts/start_server_local.sh"
 echo VM10 Server Up
 
 
