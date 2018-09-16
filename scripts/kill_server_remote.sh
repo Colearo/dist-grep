bc6346b37fcca87a246dd79b8fce84312914dc48
Kechen Lu <kechenl3@illinois.edu>
2018-09-15 19:26:07 -0500

    fix a bug
    Signed-off-by: Kechen Lu <kechenl3@illinois.edu>

--- scripts/kill_server_remote.sh
+++ scripts/kill_server_remote.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo Which VM Sever would you kill"(01-10)"?
-read vmnum
-ssh kechenl3@fa18-cs425-g29-$vmnum.cs.illinois.edu "~/go/src/dist-grep/scripts/kill_server_local.sh"
+ssh kechenl3@fa18-cs425-g29-$1.cs.illinois.edu "pkill -f server"
 
